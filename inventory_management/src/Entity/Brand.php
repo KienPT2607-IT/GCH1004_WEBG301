f4b0d6e59271fd373cf10928db812e5808838cad
Scunnn <Truongpmgch200134@fpt.edu.vn>
2022-08-15 09:12:29 +0700

    them view

--- inventory_management/src/Entity/Brand.php
+++ inventory_management/src/Entity/Brand.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\BrandRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Product;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\BrandRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: BrandRepository::class)]
 class Brand
