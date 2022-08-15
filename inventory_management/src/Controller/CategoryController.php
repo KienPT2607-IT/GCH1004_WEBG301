b99d567c90950ab0df4690de88f92745b44fa059
hanahakii <minhndgch200119@fpt.edu.vn>
2022-08-15 12:06:01 +0700

    no

--- inventory_management/src/Controller/CategoryController.php
+++ inventory_management/src/Controller/CategoryController.php
@@ -93,17 +93,6 @@ class CategoryController extends AbstractController
                     );
                     
             }
-            }
-
-            // #[Route('/search', name:'search_category')]
-            // public function search(Request $request, CategoryRepository $categoryRepository){
-            //     $key = $request->get('key');
-            //     $categories = $categoryRepository->searchCategoryByTitle($key);
-            //     return $this->render (
-            //         'category/index.html.twig',
-            //         [
-            //             'categories' => $categories
-            //         ]
-            //         );
-            // }
         }
+
+}
